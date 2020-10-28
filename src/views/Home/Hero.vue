096511a02fb8705910092278d70498007ccd2b0a
Diego Martinez <diegomartinez8995@gmail.com>
2020-10-28 03:40:52 -0600

    feat: add hero component

---
+++ src/views/Home/Hero.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="hero">
+    <img
+      class="mt-12"
+      src="../../assets/images/hero_hamburger.png"
+      alt="hamburger"
+    />
+    <div class="mt-10">
+      <h1 class="text-5xl font-bold leading-none">
+        Un nuevo sabor está en
+        <br />
+        <span class="bg-mustard">la ciudad</span>
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Hero"
+};
+</script>
+
+<style lang="postcss" scoped>
+.hero {
+  background-image: url("../../assets/images/Subtract.svg");
+  background-position: left -4rem;
+  background-size: contain;
+  background-repeat: no-repeat;
+  @apply pt-20 px-4;
+}
+</style>
--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,10 +1,14 @@
 <template>
-  <div></div>
+  <div>
+    <hero />
+  </div>
 </template>
 
 <script>
+import Hero from "./Hero.vue";
+
 export default {
   name: "Home",
-  components: {}
+  components: { Hero }
 };
 </script>
