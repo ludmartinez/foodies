e3510aa0ea098d382665a54b8f1151878af3982a
Diego Martinez <diegomartinez8995@gmail.com>
2020-10-28 01:19:57 -0600

    chore: configure theme options

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {}
+    fontFamily: {
+      syne: ["Syne", "sans-serif"]
+    },
+    extend: {
+      colors: {
+        mustard: "ffd600"
+      },
+      spacing: {
+        n1: "-0.25rem",
+        n2: "-0.5rem",
+        n3: "-0.75rem",
+        n4: "-1rem",
+        n5: "-1.25rem",
+        n6: "-1.5rem",
+        n8: "-2rem",
+        n10: "-2.5rem",
+        n12: "-3rem"
+      }
+    }
   },
   variants: {},
   plugins: []
