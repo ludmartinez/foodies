46d824ea935c5baf4d59ef0335dfd2d12c73a497
Diego Martinez <diegomartinez8995@gmail.com>
2020-10-28 16:44:00 -0600

    feat(Home): complete Hero  and apply base styles

--- src/views/Home/Hero.vue
+++ src/views/Home/Hero.vue
@@ -1,23 +1,31 @@
 <template>
   <section class="hero">
     <img
-      class="mt-12"
+      class="mt-12 w-full"
       src="../../assets/images/hero_hamburger.png"
       alt="hamburger"
     />
-    <div class="mt-10">
-      <h1 class="text-5xl font-bold leading-none">
-        Un nuevo sabor está en
-        <br />
-        <span class="bg-mustard">la ciudad</span>
+    <div class="mt-10 mx-1">
+      <h1 class="relative hero-title text-5xl font-bold leading-none">
+        Un nuevo sabor está en la ciudad
+        <span class="title-mark" />
       </h1>
+      <p class="mt-8">
+        Estamos a punto de descubrir un mundo lleno de sabores y de emociones
+        inigualables.
+      </p>
+      <f-action-btn text="Encuéntranos" class="mt-8" />
     </div>
   </section>
 </template>
 
 <script>
+import FActionBtn from "../../components/FActionBtn.vue";
+
 export default {
-  name: "Hero"
+  name: "Hero",
+
+  components: { FActionBtn }
 };
 </script>
 
@@ -27,6 +35,11 @@ export default {
   background-position: left -4rem;
   background-size: contain;
   background-repeat: no-repeat;
-  @apply pt-20 px-4;
+  @apply pt-20 pb-12 px-4;
+}
+
+.title-mark {
+  @apply absolute bg-mustard mx-n1 transform -rotate-1 w-56 h-12 left-0 bottom-0;
+  z-index: -1;
 }
 </style>
