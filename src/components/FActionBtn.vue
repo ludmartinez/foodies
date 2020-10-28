2755e4799f49e010346e40887d500a163cf06c30
Diego Martinez <diegomartinez8995@gmail.com>
2020-10-28 16:38:41 -0600

    feat(components): add FActionBtn component

---
+++ src/components/FActionBtn.vue
@@ -0,0 +1,19 @@
+<template>
+  <a class="font-syne text-xl inline-flex">
+    {{ text }}
+    <img src="../assets/icons/arrow-right.svg" alt="right arrow" class="ml-4" />
+  </a>
+</template>
+
+<script>
+export default {
+  name: "FActionBtn",
+
+  props: {
+    text: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
